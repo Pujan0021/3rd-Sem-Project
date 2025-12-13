b9fc095f4fd6c08e0d4a47eaaf97dbfeae12fb94
Pujan-Pandey <pujanpandey21@gmail.com>
2025-12-13 15:36:01 +0545

    Product Detail Page

--- Js/script.js
+++ Js/script.js
@@ -2,6 +2,7 @@ let container = document.querySelector(".mainContainer");
 let inputBox = document.querySelector(".inputBox");
 let searchBox = document.querySelector(".searchBox");
 let categoryButtons = document.querySelectorAll(".buttonForCategory");
+
 let link = "https://dummyjson.com/products";
 // console.log(categoryButtons);
 // Fetch API
@@ -25,12 +26,12 @@ const listProduct = () => {
         let items = filterProducts.map(product => {
             return `<div
             class="bg-white w-80 h-110 border-none p-5 rounded-xl shadow-2xl my-10 mx-5 transform transition duration-300 hover:scale-105"
-                >
-                <div class="flex justify-center trackCard"> <img
-                class="w-60 h-60 items-center"
-                src=${product.images[0]}
-                alt=${product.title}
-                data-id=${product.id}
+            >
+            <div class="flex justify-center trackCard"> <img
+            class="w-60 h-60 items-center"
+            src=${product.images[0]}
+            alt=${product.title}
+            data-id=${product.id}
                 /></div>
                 
                 <p class="py-2 font-bold">${product.title}</p>
@@ -57,15 +58,15 @@ searchBox.addEventListener('click', () => {
 const categoryData = (filterProducts) => {
     let items = filterProducts.map(product => {
         return `<div
-            class="bg-white w-80 h-110 border-none p-5 rounded-xl shadow-2xl my-10 mx-5 transform transition duration-300 hover:scale-105"
-                >
-                <div class="flex justify-center trackCard"> <img
-                class="w-60 h-60 items-center"
-                src=${product.images[0]}
-                alt=${product.title}
-                data-id=${product.id}
-                /></div>
-                
+        class=" bg-white w-80 h-110 border-none p-5 rounded-xl shadow-2xl my-10 mx-5 transform transition duration-300 hover:scale-105"
+        >
+        <div class="flex justify-center trackCard"> <img
+        class="w-60 h-60 items-center"
+        src=${product.images[0]}
+        alt=${product.title}
+        data-id=${product.id}
+        /></div>
+        
                 <p class="py-2 font-bold">${product.title}</p>
                 <p  class="py-2 flex justify-center gap-10"><span class="opacity-30">${product.description.slice(0, 20) + "...."}</span><button>Read More</button></p>
                 <p class="p-1 bg-gray-500  text-white  border-0 w-fit rounded-sm">${product.category}</p>
@@ -122,3 +123,42 @@ categoryButtons.forEach(element => {
 
     })
 });
+
+// Product Details
+container.addEventListener("click", (e) => {
+    if (e.target.tagName == "IMG" && e.target.dataset.id) {
+        let idOfSelectedProduct = e.target.dataset.id;
+        getData().then(products => {
+            let product = products.find(item => item.id == idOfSelectedProduct);
+            let filterProducts = [product];
+            let items = filterProducts.map(product => {
+                return `<div
+            class="bg-white w-2xl h-150 border-none p-5 rounded-xl shadow-2xl my-10 mx-5"
+            >
+            <button class="back bg-green-500 p-1 text-white rounded-sm px-2">Back</button>
+            <div class="flex justify-center trackCard"> <img
+            class="w-80 h-70 items-center"
+            src=${product.images[0]}
+            alt=${product.title}
+            data-id=${product.id}
+                /></div>
+                
+                <p class="py-2 text-2xl font-bold">${product.title}</p>
+                <p  class="py-2 flex justify-center gap-10"><span class="opacity-30">${product.description.slice(0, 150) + "...."}</span></p>
+                <p class="p-1 bg-gray-500  text-white  border-0 w-fit rounded-sm">${product.category}</p>
+                <div  class="flex py-2 justify-between">
+                <p class="text-xl">$ ${product.price}</p>
+                <button class="bg-green-500 p-1 text-white rounded-sm px-2" >Add To Cart</button>
+                </div>
+                </div>`;
+            }).join("")
+            container.innerHTML = items;
+
+            //Back Button
+            let backButton = document.querySelector(".back");
+            backButton.addEventListener("click", () => {
+                listProduct();
+            })
+        })
+    }
+})
